f2645290888585a718a6f9e13790568e30947f18
Khoa Dang <76057587+KhoaDQ@users.noreply.github.com>
2021-10-16 12:46:51 +0700

    welcome

--- index.js
+++ index.js
@@ -43,6 +43,11 @@ connection.once("open", () => {
 const exercisesRouter = require("./routes/exercises");
 const usersRouter = require("./routes/users");
 
+app.get("/", (req, res) => {
+  res.send(
+    "Welcome to my Exercise Tracker API. Go to https://apiexercisetrackernodejs.herokuapp.com/api-docs for more information"
+  );
+});
 app.use("/exercises", exercisesRouter);
 app.use("/users", usersRouter);
 
